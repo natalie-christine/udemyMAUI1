098ee607ce4e9629086f3cdb5b0694c4d58432f6
natalie-christine <161007805+natalie-christine@users.noreply.github.com>
2025-02-23 20:50:42 +0100

    Projektdateien hinzufügen.

---
+++ udemyMAUI1/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace udemyMAUI1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
