8f836a6a91bb03e069ff0eec262c4b56d87aa992
natalie-christine <161007805+natalie-christine@users.noreply.github.com>
2025-03-04 20:32:41 +0100

    update

--- udemyMAUI1/Tabb1.xaml.cs
+++ udemyMAUI1/Tabb1.xaml.cs
@@ -6,4 +6,20 @@ public partial class Tabb1 : TabbedPage
 	{
 		InitializeComponent();
 	}
+
+    private void RadioButton_CheckedChanged1(object sender, CheckedChangedEventArgs e)
+    {
+
+    }
+    private void RadioButton_CheckedChanged2(object sender, CheckedChangedEventArgs e)
+    {
+
+    }
+    private void RadioButton_CheckedChanged3(object sender, CheckedChangedEventArgs e)
+    {
+
+    }
+
+
+
 }
