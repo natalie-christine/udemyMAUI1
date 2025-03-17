1156963808f708a77853839ee4c7eed94d61aad0
natalie-christine <161007805+natalie-christine@users.noreply.github.com>
2025-03-17 22:02:03 +0100

    nimm es

--- udemyMAUI1/Page1.xaml.cs
+++ udemyMAUI1/Page1.xaml.cs
@@ -1,14 +1,81 @@
 namespace udemyMAUI1;
+using ColorUtility;
+using System;
 
 public partial class Page1 : ContentPage
 {
-	public Page1()
+
+	Random random = new Random();
+
+	List<string> quotes = new List<string>();
+
+    async Task LoadMauiAsset()
+    {
+        using var stream = await FileSystem.OpenAppPackageFileAsync("quotes.txt");
+        using var reader = new StreamReader(stream);
+
+        while (reader.Peek() !=-1)
+		{
+            quotes.Add(reader.ReadLine() ?? "nöööööööö! Jetzt nicht!");
+        }
+    }
+
+    public Page1()
 	{
 		InitializeComponent();
 	}
 
+	protected override async void OnAppearing()
+	{
+        base.OnAppearing();
+        await LoadMauiAsset();
+    }
+
 	private void Button_Clicked(object sender, EventArgs e)
 	{
         Navigation.PopAsync();
+    }
+
+	private void btnGenerateQuotes_Clicked(object sender, EventArgs e)
+	{
+        // generate random quote
+        int index = random.Next(quotes.Count);
+        quoteslabel.Text = quotes[index];
+    }
+
+	private void btnGenerateColor_Clicked(object sender, EventArgs e)
+	{
+		var startColor =
+			System.Drawing.Color.FromArgb(
+				random.Next(0, 256),
+				random.Next(0, 256),
+				random.Next(0, 256));
+
+
+        var endColor =
+            System.Drawing.Color.FromArgb(
+                random.Next(0, 256),
+                random.Next(0, 256),
+                random.Next(0, 256));
+
+
+		var colors = ColorControls.GetColorGradient(startColor, endColor, 6);
+
+		float stopOffset = .0f;
+
+		var stops = new GradientStopCollection();
+
+		foreach (var color in colors)
+		{
+			stops.Add(new GradientStop(Color.FromArgb(color.Name), stopOffset));
+			stopOffset += .2f;
+
+		}
+
+		var gradient = new LinearGradientBrush(stops, new Point(0,0), new Point (1,1));
+
+		background.Background = gradient;
+
+
     }
 }
