2f87345699c3faed8c8077158a58ec629a1e58d1
natalie-christine <161007805+natalie-christine@users.noreply.github.com>
2025-03-23 22:02:04 +0100

    go

--- udemyMAUI1/Tabb1.xaml.cs
+++ udemyMAUI1/Tabb1.xaml.cs
@@ -15,7 +15,38 @@ public partial class Tabb1 : TabbedPage, INotifyPropertyChanged
             spotlight = value;
             OnPropertyChanged();
         }
+
     }
+    public List<char> Letters
+    {
+        get => letters;
+        set
+        {
+            letters = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public string Message
+    { 
+        get => message;
+        set
+        {
+            message = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public string GameStatus
+    {
+        get => gameStatus;
+        set
+        {
+            gameStatus = value;
+            OnPropertyChanged();
+        }
+    }
+
     #endregion
 
     #region Fields
@@ -37,7 +68,11 @@ public partial class Tabb1 : TabbedPage, INotifyPropertyChanged
     };
     string answer = "";
     private string spotlight;
+    private List <char> letters = new List<char>();  
+    private string message;
     List<char> guessed = new List<char>();
+    private string gameStatus;
+    int mistakes = 0;
     #endregion
 
     #region Game Engine
@@ -52,11 +87,35 @@ public partial class Tabb1 : TabbedPage, INotifyPropertyChanged
         var temp = answer.Select(x => (guessed.IndexOf(x) >= 0 ? x : '_')).ToArray();
         Spotlight = string.Join(' ', temp);
     }
+
+    private void ResetGame(object sender, EventArgs e)
+    {
+        mistakes = 0;
+        guessed.Clear();
+        PickWord();
+        CalculateWord(answer, guessed);
+    }
+    
+    private void UpdateStatus()
+    {
+
+        if (mistakes >= 6)
+        {
+            GameStatus = "Verloren!";
+        }
+        else
+        {
+            GameStatus = "Noch " + (6 - mistakes) + " Versuche";
+        }
+    }
+
+
     #endregion
 
     public Tabb1()
 	{
 		InitializeComponent();
+        letters.AddRange("abcdefghijklmnopqrstuvwxyz".ToCharArray());
         BindingContext = this;
         PickWord();
         CalculateWord(answer, guessed);
@@ -106,4 +165,52 @@ public partial class Tabb1 : TabbedPage, INotifyPropertyChanged
     {
         candyImage.Source = picker.SelectedItem.ToString();
     }
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        var btn = sender as Button;
+        if (btn != null)
+        {
+           var letter = btn.Text;
+            btn.IsEnabled = false;
+            HandleGuess(letter[0]);
+        }
+
+    }
+
+    private void HandleGuess(char letter)
+    {
+        if(guessed.IndexOf(letter) == -1)
+        {
+            guessed.Add(letter);
+        }
+        if (answer.IndexOf(letter) >= 0)
+        {
+            CalculateWord(answer, guessed);
+            CheckIfGameWon();
+        }
+       else if (answer.IndexOf(letter) == -1)
+        {
+            mistakes++;
+            UpdateStatus();
+            CheckIfGameLost();
+
+        }
+    }
+    private void CheckIfGameWon()
+    {
+        if (Spotlight.Replace(" " , "" ) == answer)
+        {
+            Message = "Gewonnen!";
+        }
+    }
+
+    private void CheckIfGameLost()
+    {
+        if (mistakes >= 6)
+        {
+            Message = "Verloren!";
+        }
+    }
+
 }
