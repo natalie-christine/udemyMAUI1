c22a757cf63f8ca68c69a072f46eab50a3ede513
Martin Hehl <mahe@terraex.de>
2025-03-25 21:45:50 +0100

    bmi stuff

---
+++ udemyMAUI1/MVVM/ViewModels/BMIViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using udemyMAUI1.MVVM.Models;
+
+namespace udemyMAUI1.MVVM.ViewModels
+{
+    public class BMIViewModel
+    {
+        public BMI BMI { get; set; }
+
+        public BMIViewModel()
+        {
+            BMI = new BMI();
+            BMI.Height = 180;
+            BMI.Weight = 73;
+
+        }
+    }
+}
