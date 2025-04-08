eed0cd9840532837aa94be7a9b70e058d3a6ce17
Martin Hehl <mahe@terraex.de>
2025-04-08 22:15:18 +0200

    finished tasker

--- udemyMAUI1/MVVM/Views/NewTask.xaml.cs
+++ udemyMAUI1/MVVM/Views/NewTask.xaml.cs
@@ -33,4 +33,20 @@ public partial class NewTask : ContentPage
             await DisplayAlert("Error", "Please select a category", "OK");
         }
     }
+
+    private async void AddCategoryClicked(object sender, EventArgs e)
+    {
+        var vm = BindingContext as NewTaskViewModel;
+        string category = await DisplayPromptAsync("New Category", "Write the new category name", maxLength: 15, keyboard: Keyboard.Text);
+        var r = new Random();
+        if (!string.IsNullOrEmpty(category))
+        {
+            vm.Categories.Add(new Category
+            {
+                Id = vm.Categories.Max(x => x.Id) + 1,
+                Color = Color.FromRgb(r.Next(0,255), r.Next(0, 255), r.Next(0, 255)).ToHex(),
+                CategoryName = category
+            });
+        }
+    }
 }
