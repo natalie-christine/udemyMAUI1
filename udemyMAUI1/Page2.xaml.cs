b0c0eb9ee46181af144b841d40d11bd3e8ff7223
natalie-christine <161007805+natalie-christine@users.noreply.github.com>
2025-04-28 22:00:30 +0200

    color your life

--- udemyMAUI1/Page2.xaml.cs
+++ udemyMAUI1/Page2.xaml.cs
@@ -10,48 +10,12 @@ namespace udemyMAUI1
     public partial class Page2 : ContentPage, INotifyPropertyChanged
     {
 
-        Person person = new Person
-        {
-            Name = "John",
-            Phone = "1234",
-            Address = "Straﬂe 1"
-        };
 
         public Page2()
         {
             InitializeComponent();
             BMIPart.BindingContext = new BMIViewModel();
         }
-
-        override protected void OnAppearing()
-        {
-
-            base.OnAppearing();
-
-            JohnPart.BindingContext = person;
-        }
-
-        private void OnClicked(object sender, EventArgs e)
-        {
-            
-            person.Name = "Jane";
-            person.Phone = "5678";
-            person.Address = "Straﬂe 2";
-
-
-            // Variante 2
-            //txtName.BindingContext = person;
-            //txtName.SetBinding(Label.TextProperty, "Name");
-
-
-            // Variante 1
-            //Binding personBinding = new Binding();
-            //personBinding.Source = person;
-            //personBinding.Path = "Name";
-
-            //txtName.SetBinding(Label.TextProperty, personBinding);
-        }
-
     }
 
 }
